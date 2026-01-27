8c691cd636fb6786026d85b5051646fae495644b
Sigma2044 <max.serdan@outlook.de>
2026-01-27 15:13:43 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 
 // View Engine
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "Views"));
 
 // Static Files
 app.use(express.static(path.join(__dirname, "public")));
